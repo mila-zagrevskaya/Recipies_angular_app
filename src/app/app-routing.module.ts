241fca853fb7edc1b2cd6df3b36557dc7c382931
zahrevska <lyudmila.zahrevska@nixsolutions.com>
2023-03-30 18:33:47 +0200

    Adding Navigation with Event Binding and ngIf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping_list', component: ShoppingListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
