c4b2a1c07fa1598e507fc3c6ee6977ab5a4381bd
zahrevska <lyudmila.zahrevska@nixsolutions.com>
2023-10-29 17:04:05 +0100

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from "../services/recipe.service";
+
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  RECIPES_DATA_BASE_LINK = 'https://recipe-book-ng-course-e12dd-default-rtdb.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.put(this.RECIPES_DATA_BASE_LINK, recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
