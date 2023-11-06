6a377980a3c99c55ef78e21b899afb240bfa7d24
zahrevska <lyudmila.zahrevska@nixsolutions.com>
2023-11-06 13:47:06 +0100

    Adding Auto-Logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
   LOGIN_ENDPOINT: string = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDev_rd_GEcugs3NzPcOiKrUEZBBWUE4iw'
 
   user = new BehaviorSubject<|User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -89,12 +90,25 @@ export class AuthService {
 
     if(loadedUser.token) {
       this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration)
   }
 
   private handleAuthentication(
@@ -106,6 +120,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User( email, userId, token, expirationDate );
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000)
     localStorage.setItem('userData', JSON.stringify(user) );
   }
 
