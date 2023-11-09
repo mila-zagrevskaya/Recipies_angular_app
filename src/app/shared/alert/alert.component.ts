ef587694a92f00ad44a4e71a9fec3b952ed02758
zahrevska <lyudmila.zahrevska@nixsolutions.com>
2023-11-09 12:15:57 +0100

    Using ngIf

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +6,10 @@ import { Component, Input } from "@angular/core";
   styleUrls:['./alert.component.css']
 })
 export class AlertComponent {
-  @Input() message: string = '';
+  @Input() message: string;
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
 }
